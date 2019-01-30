3698c9fc35c7a2e6dca32196e2a93347331af3fa
Adam Denes <37849661+edem1993@users.noreply.github.com>
2019-01-30 23:07:28 +0100

    update

--- db_compare.py
+++ db_compare.py
@@ -19,27 +19,30 @@ def compare_db(items, dblist):
 	dbfile.seek(0)
 	db_list = dbfile.readlines() # list of db entries
 
-	"""for d in db_list:
-		for i in items:
-			if d == i:
-				print(d)
-			else:
-				pass
- 
-	print("****************\n")"""
-	
-	"""compares the unique values..
-	matches = set(items).intersection(db_list)
-	print(matches)
-	return list(matches)"""
-	
 	new_list = []
-	for element in db_list:
-		if element in items:
+	for element in items:
+		#print(element.rstrip('\n'))
+		if element in db_list:
 			new_list.append(element)
-	
-	return new_list
-	
-compare_db(o, db)
-
+			
+	result = [x.split('\t') for x in new_list]
+	return result
+	 
 print(compare_db(o, db))
+x = compare_db(o, db)
+
+def replace_list_iterations(list_elements):
+	
+	if list_elements != False:
+		for list_elem in list_elements:
+			#print(list_elem)
+			for entry in list_elem:
+				entry = entry.split()
+				
+				entry = input('enter alias IN rtype addr : ')
+				
+				print('\n' + entry + '\n')
+				return entry
+			
+			
+replace_list_iterations(x)
